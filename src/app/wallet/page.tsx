a634456be01406261ab2b22be396722520b68f28
Hritul <hritul.srivastava@gmail.com>
2024-09-15 00:00:36 +0530

    SeedArea component

---
+++ src/app/wallet/page.tsx
@@ -0,0 +1,11 @@
+import SeedArea from "@/components/SeedArea";
+
+const page = () => {
+  return (
+    <div className="relative flex flex-col items-center justify-start min-h-[75vh] p-4 gap-4">
+      <SeedArea />
+    </div>
+  );
+};
+
+export default page;
